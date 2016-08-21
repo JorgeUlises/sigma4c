505bf27a648daf390f15fce61613a2f3f18fc6b9
Fernando Pineda <wfpinedar@gmail.com>
2016-08-20 19:51:24 -0500

    Corrigiendo validación de base de datos

--- scripts/create_database_proyecto.sh
+++ scripts/create_database_proyecto.sh
@@ -2,14 +2,14 @@
 echo 'Ejecutando: create_database_proyecto.sh'
 sql1=userdbsigma4c.sql
 sql2=sigma4c.sql
+db=sigma4c
 
-
-if echo '\connect sigma4c;' | sudo psql -U postgres&>/dev/null
+if echo 'SELECT * FROM public.parametro;' | sudo psql -U postgres -d $db &>/dev/null
 then
-  echo 'La base de datos ya está creada. Nada que hacer.'
-elif echo 'SELECT * FROM public.parametro;' | sudo psql -U postgres&>/dev/null
-then 
   echo 'La tabla parametro ya está creada. Nada que hacer.'
+elif echo '\connect sigma4c;' | sudo psql -U postgres&>/dev/null
+then 
+  echo 'La base de datos ya está creada. Nada que hacer.'
 else
 
 cp $sql1 /tmp
@@ -21,6 +21,6 @@ sudo chown postgres:postgres $scriptsql2
 sudo su postgres -c "
 cd /tmp
 psql -f $scriptsql1
-psql -f $scriptsql2 -d sigma4c
+psql -f $scriptsql2 -d $db
 "
 fi
