3db01d0229a982bd1ae6cea3bef625fa5a9cddbf
Jorge Useche <naturalmentejorge@gmail.com>
2016-08-20 22:48:07 -0500

    Update sigma4c install script

--- src/install_sigma4c.sh
+++ src/install_sigma4c.sh
@@ -10,6 +10,13 @@ download accept terms of use
 osgi obr url add --url http://spring-roo-repository.springsource.org/repository.xml
 osgi obr deploy --bundleSymbolicName org.springframework.roo.wrapping.postgresql-jdbc4
 database reverse engineer --schema "public" --activeRecord false --disableGeneratedIdentifiers false --disableVersionFields true --package ~.domain
+-repository jpa --interface ~.repository.EmpresaRepository --entity ~.domain.Empresa
+-repository jpa --interface ~.repository.FuenteHidricaRepository --entity ~.domain.FuenteHidrica
+-repository jpa --interface ~.repository.MuestraRepository --entity ~.domain.Muestra
+-repository jpa --interface ~.repository.ParametroRepository --entity ~.domain.Parametro
+-repository jpa --interface ~.repository.ProyectoRepository --entity ~.domain.Proyecto
+-repository jpa --interface ~.repository.RolRepository --entity ~.domain.Rol
+-repository jpa --interface ~.repository.UsuarioRepository --entity ~.domain.Usuario
 service all --interfacePackage ~.service --classPackage ~.service.impl
 web mvc setup
 web mvc all --package ~.web
--- src/sigma4c/src/main/java/org/sigma/core/domain/ManyMuestraHasManyParametroPK.java
+++
@@ -1,6 +0,0 @@
-package org.sigma.core.domain;
-import org.springframework.roo.addon.jpa.identifier.RooIdentifier;
-
-@RooIdentifier(dbManaged = true)
-public final class ManyMuestraHasManyParametroPK {
-}
