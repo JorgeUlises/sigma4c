261621f9e684c4ad5258d626414947387010028a
Jorge Useche <naturalmentejorge@gmail.com>
2017-04-07 17:09:44 -0500

    Se pone codigo de CRUD autogenerado y se actualizan pasos.

--- src/install_sigma4c_symfony.sh
+++ src/install_sigma4c_symfony.sh
@@ -57,3 +57,59 @@ php app/console generate:doctrine:crud --entity=AppBundle:Norma
 
 #Instrucciones de migraciones:
 #https://symfonytricksandcheats.wordpress.com/category/migraciones/
+
+#Salida de la generacion de CRUD:
+# [vagrant@localhost sigma4c]$ php app/console generate:doctrine:crud --entity=AppBundle:Lectura
+#
+#
+#   Welcome to the Doctrine2 CRUD generator
+#
+#
+#
+# This command helps you generate CRUD controllers and templates.
+#
+# First, give the name of the existing entity for which you want to generate a CRUD
+# (use the shortcut notation like AcmeBlogBundle:Post)
+#
+# The Entity shortcut name [AppBundle:Lectura]:
+#
+# By default, the generator creates two actions: list and show.
+# You can also ask it to generate "write" actions: new, update, and delete.
+#
+# Do you want to generate the "write" actions [no]? yes
+#
+# Determine the format to use for the generated CRUD.
+#
+# Configuration format (yml, xml, php, or annotation) [annotation]:
+#
+# Determine the routes prefix (all the routes will be "mounted" under this
+# prefix: /prefix/, /prefix/new, ...).
+#
+# Routes prefix [/lectura]:
+#
+#
+#   Summary before generation
+#
+#
+# You are going to generate a CRUD controller for "AppBundle:Lectura"
+# using the "annotation" format.
+#
+# Do you confirm generation [yes]?
+#
+#
+#   CRUD generation
+#
+#
+# Generating the CRUD code: OK
+#
+#
+#   [RuntimeException]
+#   Unable to generate the LecturaType form class as it already exists under the /home/vagrant/src/sigma4c/src/A
+#   ppBundle/Form/LecturaType.php file
+#
+#
+# doctrine:generate:crud [--entity ENTITY] [--route-prefix ROUTE-PREFIX] [--with-write] [--format FORMAT] [--overwrite] [-h|--help] [-q|--qui
+# et] [-v|vv|vvv|--verbose] [-V|--version] [--ansi] [--no-ansi] [-n|--no-interaction] [-s|--shell] [--process-isolation] [-e|--env ENV] [--no
+# -debug] [--] <command> [<entity>]
+#
+# [vagrant@localhost sigma4c]$
---
+++ src/sigma4c/src/AppBundle/Controller/LecturaController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Lectura;
+use AppBundle\Form\LecturaType;
+
+/**
+ * Lectura controller.
+ *
+ * @Route("/lectura")
+ */
+class LecturaController extends Controller
+{
+    /**
+     * Lists all Lectura entities.
+     *
+     * @Route("/", name="lectura_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $lecturas = $em->getRepository('AppBundle:Lectura')->findAll();
+
+        return $this->render('lectura/index.html.twig', array(
+            'lecturas' => $lecturas,
+        ));
+    }
+
+    /**
+     * Creates a new Lectura entity.
+     *
+     * @Route("/new", name="lectura_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $lectura = new Lectura();
+        $form = $this->createForm('AppBundle\Form\LecturaType', $lectura);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($lectura);
+            $em->flush();
+
+            return $this->redirectToRoute('lectura_show', array('id' => $lectura->getId()));
+        }
+
+        return $this->render('lectura/new.html.twig', array(
+            'lectura' => $lectura,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Lectura entity.
+     *
+     * @Route("/{id}", name="lectura_show")
+     * @Method("GET")
+     */
+    public function showAction(Lectura $lectura)
+    {
+        $deleteForm = $this->createDeleteForm($lectura);
+
+        return $this->render('lectura/show.html.twig', array(
+            'lectura' => $lectura,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Lectura entity.
+     *
+     * @Route("/{id}/edit", name="lectura_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Lectura $lectura)
+    {
+        $deleteForm = $this->createDeleteForm($lectura);
+        $editForm = $this->createForm('AppBundle\Form\LecturaType', $lectura);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($lectura);
+            $em->flush();
+
+            return $this->redirectToRoute('lectura_edit', array('id' => $lectura->getId()));
+        }
+
+        return $this->render('lectura/edit.html.twig', array(
+            'lectura' => $lectura,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Lectura entity.
+     *
+     * @Route("/{id}", name="lectura_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Lectura $lectura)
+    {
+        $form = $this->createDeleteForm($lectura);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($lectura);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('lectura_index');
+    }
+
+    /**
+     * Creates a form to delete a Lectura entity.
+     *
+     * @param Lectura $lectura The Lectura entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Lectura $lectura)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('lectura_delete', array('id' => $lectura->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
